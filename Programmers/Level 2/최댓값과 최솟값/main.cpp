49403e9b81605bf437b218b040bc448bb2a37874
HyunJun Sung <shj9866@gmail.com>
2023-05-10 10:03:51 +0900

    최댓값과 최솟값

---
+++ Programmers/Level 2/최댓값과 최솟값/main.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+
+    string str = "";
+
+    vector<int> v;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            v.push_back(stoi(str));
+            str = "";
+        }
+        else
+            str += s[i];
+    }
+
+    if (str != "") 
+        v.push_back(stoi(str));
+
+    sort(v.begin(), v.end());
+
+    answer = to_string(v[0]) + ' ' + to_string(v[v.size() - 1]);
+
+    return answer;
+}
+
+int main()
+{
+    string t = "1 2 3 4";
+
+    solution(t);
+}
