62877c8ee49c1b5d8ae6d6ab74574ffd22cdf1d2
Hyunjun_Ezen <shj9866@gmail.com>
2023-11-13 10:36:42 +0900

    11/13 올바른 괄호

---
+++ Programmers/C#/Level 2/올바른 괄호/cs_codingTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using static System.Console;
+
+namespace cs_codingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public bool solution(string s)
+            {
+                bool answer = true;
+                int count = 0;
+
+                for( int i = 0; i < s.Length; i++)
+                {
+                    if( i == 0 && s[i] == ')')
+                    {
+                        answer = false;
+                        break;
+                    }    
+
+                    if( s[i] == '(')
+                    {
+                        count++;
+                    }
+                    else if (s[i] == ')')
+                    {
+                        count--;
+
+                        if( count < 0)
+                        {
+                            answer = false;
+                            break;
+                        }    
+                    }
+
+                    if (i == s.Length - 1 && count != 0)
+                        answer = false;
+                }
+
+                return answer;
+            }
+        }
+
+    
+    }
+}
