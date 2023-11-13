b1f0730c4d4ea32f3edf5bf9cbd968622f2138bc
Hyunjun_Ezen <shj9866@gmail.com>
2023-11-13 15:05:21 +0900

    11/13 이진 변환 반복하기

---
+++ Programmers/C#/Level 2/이진 변환 반복하기/cs_codingTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+
+namespace cs_codingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] solution(string s)
+            {
+                int[] answer = new int[2];
+                int binCount = 0;
+                int zeroCount = 0;
+                int oneCount = 0;
+
+                while( s.Length != 1)
+                {
+                    for (int i = 0; i < s.Length; i++)
+                    {
+                        if (s[i] == '0')
+                            zeroCount++;
+                        else if (s[i] == '1')
+                            oneCount++;
+                    }
+
+                    s = Convert.ToString(oneCount, 2);
+                    binCount++;
+                    oneCount = 0;
+                }
+
+                answer[0] = binCount;
+                answer[1] = zeroCount;
+
+                return answer;
+            }
+        }
+    }
+}
