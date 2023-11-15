7c312e5b49769b75818f45a75612cf1ab1358a10
HyunJun Sung <shj9866@gmail.com>
2023-11-15 20:45:16 +0900

    11/15 N개의 최소공배수

---
+++ Programmers/C#/Level 2/N개의 최소공배수/CSharp_CodingTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace CSharp_CodingTest
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int solution(int[] arr)
+            {
+                int answer = 0;
+                int lcm = 0;
+
+                for ( int i = 0; i < arr.Length - 1; i++)
+                {
+                    if( i == 0)
+                        answer = ( arr[i] * arr[ i + 1] ) / GCD(arr[i], arr[i + 1]);
+                    else
+                        answer = (answer * arr[i + 1]) / GCD(answer, arr[i + 1]);
+                }
+
+                return answer;
+            }
+
+            int GCD (int a , int b )
+            {
+                int temp = 0;
+
+                while( b != 0)
+                {
+                    temp = a % b;
+                    a = b;
+                    b = temp;
+                }
+
+                return a;
+            }
+
+            int[] a = { 2, 6, 8, 14 };
+
+            WriteLine(solution(a));
+        }
+    }
+}
