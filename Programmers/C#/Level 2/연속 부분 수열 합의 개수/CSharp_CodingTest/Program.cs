33b754445249e2c45e1f60495e7e91ffec69e25b
HyunJun Sung <shj9866@gmail.com>
2023-11-15 22:58:38 +0900

    11/15 연속 부분 수열 합의 개수

---
+++ Programmers/C#/Level 2/연속 부분 수열 합의 개수/CSharp_CodingTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace CSharp_CodingTest
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int solution(int[] elements)
+            {
+                int answer = 0;
+                int[] elementSum = new int[elements.Length * 1000 + 1];
+
+                // 숫자 길이별 체크
+                for ( int length = 1; length <= elements.Length; length++ )
+                {
+                    for( int i = 0; i < elements.Length; i++)
+                    {
+                        int sum = 0;
+
+                        for (int j = 0; j < length; j++)
+                        {
+                            sum += elements[(i + j) % (elements.Length)];
+                        }
+
+                        if(elementSum[sum] == 0)
+                        {
+                            elementSum[sum]++;
+                            answer++;
+                        }
+                    }
+                }
+                
+
+                return answer;
+            }
+        }
+    }
+}
