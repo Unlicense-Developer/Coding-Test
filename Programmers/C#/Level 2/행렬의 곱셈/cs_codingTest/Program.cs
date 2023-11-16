43f265df7a21016ea2f9dde56a0a4f3cc88e967c
Hyunjun_Ezen <shj9866@gmail.com>
2023-11-16 15:28:43 +0900

    11/16 행렬의 곱셈

---
+++ Programmers/C#/Level 2/행렬의 곱셈/cs_codingTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace cs_codingTest
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[,] solution(int[,] arr1, int[,] arr2)
+            {
+                int[,] answer = new int[arr1.GetLength(0), arr2.GetLength(1)]; ;
+                
+                //행
+                for( int i = 0; i < answer.GetLength(0); i++)
+                {
+                    //열
+                    for( int j = 0; j < answer.GetLength(1); j++)
+                    {
+                        int count = 0;
+
+                        while( count < arr2.GetLength(0))
+                        {
+                            answer[i, j] += arr1[i, count] * arr2[count, j];
+                            count++;
+                        }
+                    }
+                }
+                
+                return answer;
+            }
+        }
+    }
+}
