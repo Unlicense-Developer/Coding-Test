103001f56dc1b280248a2740de2229e3aa9a689a
Hyunjun_Ezen <shj9866@gmail.com>
2023-11-16 12:44:56 +0900

    11/16 H-Index

---
+++ Programmers/C#/Level 2/H-Index/cs_codingTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using static System.Console;
+
+namespace cs_codingTest
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int solution(int[] citations)
+            {
+                int answer = 0;
+                int sum = 0;
+
+                for( int i = 0; i < citations.Length; i++)
+                {
+                    sum += citations[i];
+                }
+
+                if(sum % citations.Length == 0)
+                    answer = sum / citations.Length;
+                else
+                {
+                    answer = sum / citations.Length;
+
+                    for( int i = answer; i > 0; i--)
+                    {
+                        int count = 0;
+
+                        for (int j = 0; j < citations.Length; j++)
+                        {
+                            if (i <= citations[j])
+                                count++;
+                        }
+
+                        if (count >= i)
+                        {
+                            answer = i;
+                            break;
+                        }
+                    }
+                }
+
+                return answer;
+            }
+        }
+    }
+}
