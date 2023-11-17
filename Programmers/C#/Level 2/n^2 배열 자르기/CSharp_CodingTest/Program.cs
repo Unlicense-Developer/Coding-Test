6340d135196b9ad64a2d21a51120b200090648fd
HyunJun Sung <shj9866@gmail.com>
2023-11-17 23:15:57 +0900

    11/17 n^2 배열 자르기 미완

---
+++ Programmers/C#/Level 2/n^2 배열 자르기/CSharp_CodingTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace cs_codingTest
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] solution(int n, long left, long right)
+            {
+                int[] answer = new int[right - left + 1];
+
+                for (int i = 0; i < answer.Length; i++)
+                {
+                    int num = i + (int)left;
+
+                    answer[i] = (num / n >= num % n) ? num / n : num % n;
+                    answer[i] += 1;
+                }
+
+                return answer;
+            }
+        }
+    }
+}
