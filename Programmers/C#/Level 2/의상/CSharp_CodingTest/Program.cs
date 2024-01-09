c6036acaac38830921bcce67310cece25dfd05ae
HyunJun Sung <shj9866@gmail.com>
2024-01-09 23:33:43 +0900

    의상

---
+++ Programmers/C#/Level 2/의상/CSharp_CodingTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cs_codingTest
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int solution(string[,] clothes)
+            {
+                int answer = 1;
+
+                Dictionary<string, int> clothesCount = new Dictionary<string, int>();
+
+                for (int i = 0; i < clothes.GetLength(0); i++)
+                {
+                    string type = clothes[i, 1]; // 옷 종류 string 
+                    if(clothesCount.ContainsKey(type))
+                    {
+                        clothesCount[type]++;
+                    }
+                    else
+                    {
+                        clothesCount[type] = 1;
+                    }
+                }
+
+                foreach ( int count in clothesCount.Values)
+                {
+                    answer *= ( count + 1 );
+                }
+
+                answer--;
+
+                return answer;
+            }
+        }
+    }
+}
